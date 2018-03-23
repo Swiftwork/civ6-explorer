f5b8ff6648893f58ac4e84d774fbe4c29b836f3d
Johan Nygren <Johan.Nygren@evry.com>
2018-03-24 00:23:15 +0100

    Feat: some parsing

--- src/app/tree/tree.component.ts
+++ src/app/tree/tree.component.ts
@@ -12,20 +12,93 @@ import { XmlReader } from '../services/xmlreader';
 export class TreeComponent implements OnInit {
 
   public nodes: TreeNode[] = Civics;
+  public jsonCivics: TreeNode[];
 
   private rows = 9;
+
+  public exampleCivic: TreeNode = new TreeNode('CIVIC_CODE_OF_LAWS', 'LOC_CIVIC_CODE_OF_LAWS_NAME', '', 20, 'ADVISOR_GENERIC', Era.ERA_ANCIENT, 0, [], '');
 
   constructor(private xmlReader: XmlReader) { }
 
   ngOnInit() {
-    let civicsJson;
-    this.xmlReader.read('/assets/data/BaseGame/Civics.xml').subscribe((data) => {
-      civicsJson = data;
+    this.xmlReader.read('/assets/data/BaseGame/Civics.xml').subscribe((data: ICivicsJson) => {
+      console.log('civics from json', data);
+
+      for (let i = 0; i < data.GameInfo.Civics.Row.length; i++) {
+        let civicrow = data.GameInfo.Civics.Row[i] as ICivicRow;
+        this.jsonCivics.push(new TreeNode(
+          civicrow.CivicType,
+          civicrow.Name,
+          'Description',
+          +civicrow.Cost,
+          civicrow.AdvisorType,
+          this.getEraType(civicrow.EraType),
+          +civicrow.UITreeRow + 3,
+          ['Prereq'],
+          'Boost',
+          false,
+        ));
+        console.log(i);
+      }
+      console.log('civics parsed', this.jsonCivics);
     });
-    console.log('civics', civicsJson);
+    console.log(this.nodes);
+  }
+
+  private getEraType(eratype: string): Era {
+    switch (eratype) {
+      case 'ERA_INFORMATION':
+        return Era.ERA_INFORMATION;
+      case 'ERA_ATOMIC':
+        return Era.ERA_ATOMIC;
+      case 'ERA_MODERN':
+        return Era.ERA_MODERN;
+      case 'ERA_INDUSTRIAL':
+        return Era.ERA_INDUSTRIAL;
+      case 'ERA_RENAISSANCE':
+        return Era.ERA_RENAISSANCE;
+      case 'ERA_MEDIEVAL':
+        return Era.ERA_MEDIEVAL;
+      case 'ERA_CLASSICAL':
+        return Era.ERA_ANCIENT;
+      case 'ERA_ANCIENT':
+      default:
+        return Era.ERA_ANCIENT;
+    }
   }
 
   public layout(row: number, cost: number, era: Era) {
   }
 
+}
+
+interface ICivicsJson {
+  GameInfo: {
+    Boosts: IRow,
+    CivicModifiers: IRow,
+    CivicPrereqs: IRow,
+    CivicQuotes: IRow,
+    Civics: IRow,
+    Kinds: IRow,
+    ModifierArguments: IRow,
+    Modifiers: IRow,
+    RequirementArguments: IRow,
+    RequirementSetRequirements: IRow,
+    RequirementSets: IRow,
+    Requirements: IRow,
+    Types: IRow,
+  }
+}
+
+interface ICivicRow {
+  AdvisorType: string,
+  CivicType: string,
+  Cost: string,
+  EraType: string,
+  Name: string,
+  UITreeRow: string,
+}
+
+interface IRow {
+  Row: any[],
 }
